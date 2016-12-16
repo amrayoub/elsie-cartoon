81f217b7a25fc053ee928cfaeb7fa157ee6ee619
matt <matthewp851@gmail.com>
2016-12-15 19:49:51 -0600

    pakInsert still undefined in Contacts...

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,15 +1,50 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { Pako } from '../../providers/pako';
+
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
+
 export class ContactPage {
+  record: any;
+  rander: any[];
+  keys: any[];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public pako: Pako) {
+    
+  }
+  makeVal() {
+    let rander = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'l', 'm', 'n', 'o', 'p'];
+    return rander[(Math.random() * rander.length) | 0]
+  }
+  makeKey() {
+    return new Date().valueOf().toString();
+  }
 
+  pInsert(key, val) {
+    val = this.makeVal(); // override fake dev data
+    key = this.makeKey(); // override fake dev data
+    this.pako.pakInsert(key, val)
+      .then((val) => {
+        console.log("Inserted " + val);
+        this.record = val;
+      });
+  }
+
+  pKeys() {
+    this.pako.pakKeys()
+      .then((val) => {
+        this.keys = val;
+      })
+  }
+
+  pRead(key, val) {
+    this.pako.pakRead(key).then((val) => {
+      this.record = val;
+    });
   }
 
 }
--- src/providers/pako.ts
+++ src/providers/pako.ts
@@ -1,18 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http } from '@angular/http'; 
+import { Storage } from '@ionic/storage';
 import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the Pako provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class Pako {
   data: any;
 
-  constructor(public http: Http) {
+  constructor(public http: Http, public db: Storage) {
     console.log('Hello Pako Provider');
     this.data = [
       { name: 'Microsoft', code: 324, product: 'Windows 10' },
@@ -21,14 +16,49 @@ export class Pako {
       { name: 'Oracle', code: 89, product: 'RDBMS' },
       { name: 'IBM', code: 542, product: 'Computer Hardware and Software' }
     ];
+    
   }
 
   loadAll() {
     return Promise.resolve(this.data)
-      .catch((err) => {
-        console.log('error in this.data ' + JSON.stringify(err))
-      });
+      .catch((err) => { console.log('error in this.data ' + JSON.stringify(err)); });
   }
+
+  public pakInsert(key, val): any {
+    this.db.set(key, val)
+      .then((val) => { return val })
+      .catch((err) => { console.log('pakInsert error ' + err) })
+  }
+
+  pakRead(key): any {
+    this.db.get(key)
+      .then((val) => { return val })
+      .catch((err) => { console.log('pakRead error ' + err) })
+  }
+
+  pakKeys(): any {
+    this.db.keys()
+      .then((val) => { return val })
+      .catch((err) => { console.log('pakKeys error ' + err) })
+  }
+
+  // pakRead(key) {
+  //   this.db.get(key)
+  //     .then(function (val) {
+  //       console.log(val);
+  //       return Promise.resolve(val)
+  //         .catch((err) => { console.log('error in pakREad ' + JSON.stringify(err)); });
+  //     }).catch(function (err) { console.log(err); });
+  // }
+
+  // pakKeys() {
+  //   this.db.keys().then(function (keys) {
+  //     console.log(keys);
+  //     return Promise.resolve(keys)
+  //       .catch((err) => { console.log('error in pakKeys ' + JSON.stringify(err)); });
+  //   }).catch(function (err) { console.log(err); });
+  // }
+
 
 
 } // Pako
